1cafa4e10c0324b6e81ccfd247630f1ef8a8a500
Adam Fox <adam@adamfox.com>
2019-11-18 17:20:28 -0600

    Proper dark mode. Fixed nav.

--- js/adamfox.js
+++ js/adamfox.js
@@ -1,15 +1,15 @@
-$("#btn-theme").click(function() {
-	activateDarkMode();
-});
+var colorTheme;
 
 function activateDarkMode() {
+	colorTheme = "dark";
 	const rootElement = document.querySelector(':root')
 	const darkTheme = {
 		'--primary-color': '32, 34, 44',
 		'--secondary-color': '240, 246, 250',
 		'--tertiary-color': '16, 17, 20',
 		'--skills-desktop': '7%',
-		'--box-shadow': '0px 50px 100px #000'
+		'--box-shadow': '0px 50px 100px #000',
+		'--icon-mode': 'url("../img/ico-day.svg")'
 	}
 	for(k in darkTheme) {
 	  rootElement.style.setProperty(k, darkTheme[k])
@@ -17,13 +17,15 @@ function activateDarkMode() {
   }
   
   function activateLightMode() {
+	  colorTheme = "light";
 	const rootElement = document.querySelector(':root')
 	const lightTheme = {
-		'--primary-color': '255, 255, 255',
-		'--secondary-color': '32, 34, 44',
-		'--tertiary-color': '16, 17, 20',
+		'--primary-color': '244, 246, 247',
+		'--secondary-color': '57, 61, 79',
+		'--tertiary-color': '249, 251, 252',
 		'--skills-desktop': '7%',
-		'--box-shadow': '0px 50px 100px rgba(0, 0, 0, .5'
+		'--box-shadow': '0px 50px 100px rgba(57, 61, 79, .1)',
+		'--icon-mode': 'url("../img/ico-night.svg")'
 	}
 	for(k in lightTheme) {
 	  rootElement.style.setProperty(k, lightTheme[k])
@@ -46,7 +48,6 @@ function activateDarkMode() {
 	if(isDarkMode) activateDarkMode()
 	if(isLightMode) activateLightMode()
 	if(isNotSpecified || hasNoSupport) {
-	  console.log('You specified no preference for a color scheme or your browser does not support it. I schedule dark mode during night time.')
 	  now = new Date();
 	  hour = now.getHours();
 	  if (hour < 4 || hour >= 16) {
@@ -57,4 +58,29 @@ function activateDarkMode() {
 
 $( document ).ready(function() {
     setColorScheme();
+});
+
+$("#btn-theme").click(function() {
+	if (colorTheme == "dark") {
+		activateLightMode();
+	} else {
+		activateDarkMode();
+	}
+});
+
+$("header > div > svg").click(function() {
+	$("html, body").animate({ scrollTop: "0px" });
+});
+
+$(window).scroll(function() {	
+	if($("header").offset().top - $(window).scrollTop() <= 0) {
+		$("header").addClass("on");
+		$("header").removeClass("off");
+	} 
+	if($(window).scrollTop() <= 0) {
+		if($("header").hasClass("on")) {
+			$("header").removeClass("on");
+			$("header").addClass("off");
+		}
+	}
 });
